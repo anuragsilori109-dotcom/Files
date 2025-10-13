49089c4479d53dbd7d2459af3ddf6b26de9e9178
anuragsilori109-dotcom <anuragsilori109@gmail.com>
2025-10-13 16:35:33 +0530

    Update High Followers.py

--- High Followers.py
+++ High Followers.py
@@ -21,32 +21,33 @@ import random
 from rich.console import Console
 from art import text2art
 import time
+import base64
+import uuid
+import platform
+import hashlib
+import sys
+from datetime import datetime
+
 console = Console()
+
 COLOR_COMBOS = [
     ['cyan', 'yellow'],
     ['magenta', 'red'],
     ['blue', 'bright_cyan'],
     ['white', 'bright_black'],
 ]
+
 banner = text2art("BAYMAX!", "block")
 colors = random.choice(COLOR_COMBOS)
+
 for i, line in enumerate(banner.splitlines()):
     color = colors[i % len(colors)]
     console.print(line.center(console.width), style=color)
+
 main_color = colors[0]
 accent_color = colors[1]
-import base64
-import uuid
-import platform
-import hashlib
-import base64
-import pytz
-import requests
-import sys
-from datetime import datetime
-main_color = colorrandoms[0]
-accent_color = colorrandoms[1]
-console.print("\n" + "HIGH FOLLOWERS FILE".center(term_width), style=f"bold {main_color}")
+
+console.print("\n" + "HIGH FOLLOWERS FILE".center(console.width), style=f"bold {main_color}")
 console.print(f"\n[bold {accent_color}]Enter Your Tele Token : [/bold {accent_color}]", end="")
 Token = input()
 console.print(f"[bold {accent_color}]Enter Your User ID    : [/bold {accent_color}]", end="")
@@ -346,6 +347,7 @@ minimum_followers =30
 minimum_posts =2
 
 for _ in range(120):Thread(target=gg,args=(minimum_followers,minimum_posts,generate_user_id)).start()
+
 
 
 
